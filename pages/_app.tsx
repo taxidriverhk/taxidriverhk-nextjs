e125bdd7edc356846c207a3288d7dc50235b4d85
Alex Leung <taxidriverhk@gmail.com>
2024-03-27 23:52:14 -0700

    Added notice about server migration on the production side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import NextNProgress from "nextjs-progressbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shared/styles/globals.css";
 
+import Alert from "react-bootstrap/Alert";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -16,6 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
           showSpinner: false,
         }}
       />
+      <Alert variant="warning">
+        We are in the process of migrating the server. Please use the server at{" "}
+        <a href="https://beta.taxidriverhk.com">
+          https://beta.taxidriverhk.com
+        </a>{" "}
+        to try it out.
+      </Alert>
       <Component {...pageProps} />
     </>
   );
