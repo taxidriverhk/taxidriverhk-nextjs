07b0592e93d9135fdab75ae613bab01dd2ec4ec8
Alex Leung <taxidriverhk@gmail.com>
2024-03-27 23:55:53 -0700

    Added notice about server migration on the development side

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import NextNProgress from "nextjs-progressbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shared/styles/globals.css";
 
+import Alert from "react-bootstrap/Alert";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -16,6 +18,12 @@ export default function App({ Component, pageProps }: AppProps) {
           showSpinner: false,
         }}
       />
+      <Alert variant="danger">
+        This website is deployed into a new server which is still being
+        configured, so you may find it unstable at times. Please use{" "}
+        <a href="https://taxidriverhk.com">https://taxidriverhk.com</a> if you
+        are encountering problems at here.
+      </Alert>
       <Component {...pageProps} />
     </>
   );
