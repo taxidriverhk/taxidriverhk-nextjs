b0167c6689a2ef82992d42d5a1a9c62be80b3295
Alex Leung <taxidriverhk@gmail.com>
2026-05-16 23:15:19 -0700

    Migrate hkadbus2 API to Fastify

--- shared/config/hkadbus2-config.ts
+++ shared/config/hkadbus2-config.ts
@@ -1,12 +1,5 @@
-const DEV_API_ENDPOINT = "http://localhost:8081/hkadbus2/api";
-const PROD_API_ENDPOINT = "http://127.0.0.1:8080/hkadbus2/api";
+import { getApiEndpoint as getFastifyApiEndpoint } from "shared/config/fastify-config";
 
 export function getApiEndpoint(): string {
-  const endpointOverride = process.env.HKADBUS2_API_ENDPOINT;
-  const isDev = process.env.NODE_ENV === "development";
-
-  if (endpointOverride != null) {
-    return endpointOverride;
-  }
-  return isDev ? DEV_API_ENDPOINT : PROD_API_ENDPOINT;
+  return `${getFastifyApiEndpoint()}/hkadbus2`;
 }
